aa1afdbd5ea70156039a8bea90de95571eb3b78c
csrlnd <cs.roland0123@gmail.com>
2022-06-10 00:02:43 +0200

    Upload project

---
+++ EnumsBenchTest/Benchmarks.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using FastEnumUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsBenchTest
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        [Benchmark]
+        public String EnumToString()
+        {
+            return Color.Lime.ToString();
+        }
+
+        [Benchmark]
+        public bool EnumIsDefined()
+        {
+            return Enum.IsDefined(typeof(Color), Color.Lime);
+        }
+
+        [Benchmark]
+        public bool EnumCouldParse()
+        {
+            return Enum.TryParse("Lime", false, out Color type);
+        }
+
+        [Benchmark]
+        public String EnumToStringFast()
+        {
+            return FastEnum.GetName<Color>(Color.Lime);
+        }
+
+        [Benchmark]
+        public bool EnumIsDefinedFast()
+        {
+            return FastEnum.IsDefined<Color>(Color.Lime);
+        }
+
+        [Benchmark]
+        public bool EnumCouldParseFast()
+        {
+            return FastEnum.TryParse<Color>("Lime", out Color type);
+        }
+    }
+}
---
+++ EnumsBenchTest/Color.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsBenchTest
+{
+    enum Color
+    {
+        ActiveBorder,
+
+        ActiveCaption,
+
+        ActiveCaptionText,
+
+        AliceBlue,
+
+        AntiqueWhite,
+
+        AppWorkspace,
+
+        Aqua,
+
+        Aquamarine,
+
+        Azure,
+
+        Beige,
+
+        Bisque,
+
+        Black,
+
+        BlanchedAlmond,
+
+        Blue,
+
+        BlueViolet,
+
+        Brown,
+
+        BurlyWood,
+
+        ButtonFace,
+
+        ButtonHighlight,
+
+        ButtonShadow,
+
+        CadetBlue,
+
+        Chartreuse,
+
+        Chocolate,
+
+        Control,
+
+        ControlDark,
+
+        ControlDarkDark,
+
+        ControlLight,
+
+        ControlLightLight,
+
+        ControlText,
+
+        Coral,
+
+        CornflowerBlue,
+
+        Cornsilk,
+
+        Crimson,
+
+        Cyan,
+
+        DarkBlue,
+
+        DarkCyan,
+
+        DarkGoldenrod,
+
+        DarkGray,
+
+        DarkGreen,
+
+        DarkKhaki,
+
+        DarkMagenta,
+
+        DarkOliveGreen,
+
+        DarkOrange,
+
+        DarkOrchid,
+
+        DarkRed,
+
+        DarkSalmon,
+
+        DarkSeaGreen,
+
+        DarkSlateBlue,
+
+        DarkSlateGray,
+
+        DarkTurquoise,
+
+        DarkViolet,
+
+        DeepPink,
+
+        DeepSkyBlue,
+
+        Desktop,
+
+        DimGray,
+
+        DodgerBlue,
+
+        Firebrick,
+
+        FloralWhite,
+
+        ForestGreen,
+
+        Fuchsia,
+
+        Gainsboro,
+
+        GhostWhite,
+
+        Gold,
+
+        Goldenrod,
+
+        GradientActiveCaption,
+
+        GradientInactiveCaption,
+
+        Gray,
+
+        GrayText,
+
+        Green,
+
+        GreenYellow,
+
+        Highlight,
+
+        HighlightText,
+
+        Honeydew,
+
+        HotPink,
+
+        HotTrack,
+
+        InactiveBorder,
+
+        InactiveCaption,
+
+        InactiveCaptionText,
+
+        IndianRed,
+
+        Indigo,
+
+        Info,
+
+        InfoText,
+
+        Ivory,
+
+        Khaki,
+
+        Lavender,
+
+        LavenderBlush,
+
+        LawnGreen,
+
+        LemonChiffon,
+
+        LightBlue,
+
+        LightCoral,
+
+        LightCyan,
+
+        LightGoldenrodYellow,
+
+        LightGray,
+
+        LightGreen,
+
+        LightPink,
+
+        LightSalmon,
+
+        LightSeaGreen,
+
+        LightSkyBlue,
+
+        LightSlateGray,
+
+        LightSteelBlue,
+
+        LightYellow,
+
+        Lime,
+
+        LimeGreen,
+
+        Linen,
+
+        Magenta,
+
+        Maroon,
+
+        MediumAquamarine,
+
+        MediumBlue,
+
+        MediumOrchid,
+
+        MediumPurple,
+
+        MediumSeaGreen,
+
+        MediumSlateBlue,
+
+        MediumSpringGreen,
+
+        MediumTurquoise,
+
+        MediumVioletRed,
+
+        Menu,
+
+        MenuBar,
+
+        MenuHighlight,
+
+        MenuText,
+
+        MidnightBlue,
+
+        MintCream,
+
+        MistyRose,
+
+        Moccasin,
+
+        NavajoWhite,
+
+        Navy,
+
+        OldLace,
+
+        Olive,
+
+        OliveDrab,
+
+        Orange,
+
+        OrangeRed,
+
+        Orchid,
+
+        PaleGoldenrod,
+
+        PaleGreen,
+
+        PaleTurquoise,
+
+        PaleVioletRed,
+
+        PapayaWhip,
+
+        PeachPuff,
+
+        Peru,
+
+        Pink,
+
+        Plum,
+
+        PowderBlue,
+
+        Purple,
+
+        RebeccaPurple,
+
+        Red,
+
+        RosyBrown,
+
+        RoyalBlue,
+
+        SaddleBrown,
+
+        Salmon,
+
+        SandyBrown,
+
+        ScrollBar,
+
+        SeaGreen,
+
+        SeaShell,
+
+        Sienna,
+
+        Silver,
+
+        SkyBlue,
+
+        SlateBlue,
+
+        SlateGray,
+
+        Snow,
+
+        SpringGreen,
+
+        SteelBlue,
+
+        Tan,
+
+        Teal,
+
+        Thistle,
+
+        Tomato,
+
+        Transparent,
+
+        Turquoise,
+
+        Violet,
+
+        Wheat,
+
+        White,
+
+        WhiteSmoke,
+
+        Window,
+
+        WindowFrame,
+
+        WindowText,
+
+        Yellow,
+
+        YellowGreen
+
+    }
+}
